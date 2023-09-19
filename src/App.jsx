4dfdf4f1fd540f45d3dceaa86d66ad28c6fa6c4b
Davi Silva <gssj9264@gmail.com>
2023-09-19 17:19:35 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <input type="text" className="CaixaPesquisa" placeholder="Pesquise no Universi.Me"></input>
+      
+
+      </header>
+
+        <button className="custom-button">Search</button>
+      <div className="Quadrado"></div>
+    </div>
+  );
+}
+
+export default App;
