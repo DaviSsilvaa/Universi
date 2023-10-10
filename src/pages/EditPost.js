4a2c27b7be1ea2c95585848a8eaa406b44b8a689
SilvioNascimento <silvio.estudante19@gmail.com>
2023-10-09 23:14:35 -0300

    Desenvolvendo o EditPost.js para editar os posts

--- src/pages/EditPost.js
+++ src/pages/EditPost.js
@@ -1,44 +1,66 @@
 import React, { useEffect, useState } from "react";
-import {Link, useParams} from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import axios from "axios";
 
-
-
 const EditPost = () => {
+  const { changeId } = useParams();
+  const [changeMessage, setChangeMessage] = useState("");
+  const [post, setPost] = useState(null);
 
-    const { changeId } = useParams()
-    const [changeMessage, setChangeMessage] = useState()
-
-    const putPost = async (changeId) => {
-        const post = {changeMessage}
-        await axios.put(`http://localhost:8080/api/posts/${changeId}`, post)
+  useEffect(() => {
+    async function fetchPost() {
+      try {
+        const response = await axios.get(`http://localhost:8080/api/posts/${changeId}`);
+        setPost(response.data);
+        setChangeMessage(response.data.postMessage);
+      } catch (error) {
+        console.error("Erro ao buscar o post:", error);
+      }
     }
 
-    useEffect(() => {
-        axios.get(`http://localhost:8080/api/posts/${changeId}`)
-    })
+    fetchPost();
+  }, [changeId]);
 
-    return (
-        <div className="new-post">
-        <h2>Comente sobre algo</h2>
-        <form onSubmit={(changeId) => putPost(changeId)}>
-            <div className = 'forms-control'>
-                <label htmlFor='text'>Mensagem</label>  
-                <textarea id='text'
-                          name='text'
-                          placeholder='Digite a mensagem'
-                          onChange={(changeId) => setChangeMessage(changeId.target.value)}
-                />
-            </div>
-            <input type='submit' value='criar post' className='button'/>
+  const putPost = async (e) => {
+    e.preventDefault();
+
+    const updatedPost = { postMessage: changeMessage };
+
+    try {
+      await axios.put(`http://localhost:8080/api/posts/${changeId}`, updatedPost);
+      // Redirecionar após a atualização (por exemplo, para a página do post)
+    } catch (error) {
+      console.error("Erro ao atualizar o post:", error);
+    }
+  }
+
+  return (
+    <div className="new-post">
+      <h2>Editar Post</h2>
+      {post ? (
+        <form onSubmit={putPost}>
+          <div className='forms-control'>
+            <label htmlFor='text'>Mensagem</label>
+            <textarea
+              id='text'
+              name='text'
+              placeholder='Digite a mensagem'
+              value={changeMessage}
+              onChange={(e) => setChangeMessage(e.target.value)}
+            />
+          </div>
+          <input type='submit' value='Atualizar post' className='button' />
         </form>
-        <div className='btn-back'>
-            <Link to='/'>
-                <button>Voltar para o feed</button>
-            </Link>
-        </div>
+      ) : (
+        <p>Carregando post...</p>
+      )}
+      <div className='btn-back'>
+        <Link to='/'>
+          <button>Voltar para o feed</button>
+        </Link>
+      </div>
     </div>
-    )
+  );
 }
 
 export default EditPost;
