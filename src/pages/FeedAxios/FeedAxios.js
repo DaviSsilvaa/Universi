996b6e224fbc9dd361879ab9105e5300d130bb03
SilvioNascimento <silvio.estudante19@gmail.com>
2023-11-04 23:20:43 -0300

    Adicionar uma quebra de linha automático

--- src/pages/FeedAxios/FeedAxios.js
+++ src/pages/FeedAxios/FeedAxios.js
@@ -1,33 +1,26 @@
-import { useState, useEffect, useContext} from "react";
-import React from "react";
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import './FeedAxios.css';
 import { AppContext } from "../../App";
 
+const FeedAxios = () => {
+    const { username, token } = useContext(AppContext);
 
-const FeedAxios= () => {
+    const [post, setPost] = useState([]);
 
-    const {username, token} = useContext(AppContext);
-
-    const [post, setPost] = useState([])
-
-    const getPosts = async() => {
-
+    const getPosts = async () => {
         try {
-            const response = await axios.get('http://localhost:8080/api/posts')
-
-            const data = response.data
-            setPost(data)
-
-        } catch (e){
-            console.log(e)
+            const response = await axios.get('http://localhost:8080/api/posts');
+            const data = response.data;
+            setPost(data);
+        } catch (e) {
+            console.log(e);
         }
-
     }
 
     useEffect(() => {
-        getPosts()
+        getPosts();
     }, []);
 
     function deletePost(id) {
@@ -36,60 +29,65 @@ const FeedAxios= () => {
                 Authorization: `Bearer ${token}`
             }
         })
-        setPost(post.filter(post => post._id !== id ))
-        window.location.reload();
+            .then(() => {
+                setPost(prevPost => prevPost.filter(p => p.id !== id));
+            })
+            .catch(error => {
+                console.log("Erro ao excluir post:", error);
+            });
     }
-    
-    
+
+    function formatTextWithLineBreaks(text, maxLength) {
+        let formattedText = '';
+
+        for (let i = 0; i < text.length; i += maxLength) {
+            formattedText += text.slice(i, i + maxLength) + '\n';
+        }
+
+        return formattedText;
+    }
 
     return (
         <div className="post">
-
             <p>Bem vindo, {username}</p>
-
             <div className="boxPosts"></div>
             <div className="">
                 <Link to='/post'>
                     <button className="btn-newPost">Nova Publicação</button>
                 </Link>
             </div>
-
-            
-            {post.length === 0 ? (<p className="textboxuser">Carregando...</p>) : (
-                post.map((post) => (
-                    
-                    <div className='BoxPostUser' key={post.id}>
-                        <p>{post.user.login}</p>
+            {post.length === 0 ? (
+                <p className="textboxuser">Carregando...</p>
+            ) : (
+                post.map((p) => (
+                    <div className='BoxPostUser' key={p.id}>
+                        <p>{p.user.login}</p>
                         <div className="ContentPost">
-                            {post.message.split('\n').map((line, index) => (
+                            {formatTextWithLineBreaks(p.message, 47).split('\n').map((line, index) => (
                                 <React.Fragment key={index}>
                                     {index > 0 && <br />}
                                     {line}
                                 </React.Fragment>
                             ))}
                         </div>
-                        {post.user.login === username && 
-                        <div className="btns">
-                            <div className="btn-edit">
-                                <Link to={{ pathname: `/edit/${post.id}` }}>
-                                    <button>Editar</button>
-                                </Link>
+                        {p.user.login === username &&
+                            <div className="btns">
+                                <div className="btn-edit">
+                                    <Link to={{ pathname: `/edit/${p.id}` }}>
+                                        <button>Editar</button>
+                                    </Link>
+                                </div>
+                                <div className="btn-delete">
+                                    <Link to='/feed'>
+                                        <button onClick={() => deletePost(p.id)}>Deletar</button>
+                                    </Link>
+                                </div>
                             </div>
-
-                            <div className="btn-delete">
-                                <Link to='/feed'>
-                                    <button onClick={() => deletePost(post.id) }>Deletar</button>
-                                </Link>
-                            </div>
-                        </div>
                         }
                     </div>
-                
                 ))
             )}
         </div>
-
-
     );
 };
 
