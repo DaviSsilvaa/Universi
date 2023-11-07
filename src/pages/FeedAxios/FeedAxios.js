9047900c740c47b35f74dee7951f9337377e6400
SilvioNascimento <silvio.estudante19@gmail.com>
2023-11-07 17:06:13 -0300

    Consertando a lógica e estilizando o botão LerMais

--- src/pages/FeedAxios/FeedAxios.js
+++ src/pages/FeedAxios/FeedAxios.js
@@ -1,33 +1,30 @@
-import { useState, useEffect, useContext} from "react";
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect, useContext, useRef } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import './FeedAxios.css';
+import Swal from 'sweetalert2';
 import { AppContext } from "../../App";
 
+const FeedAxios = () => {
+    const { username, token } = useContext(AppContext);
 
-const FeedAxios= () => {
+    const [post, setPost] = useState([]);
 
-    const {username, token} = useContext(AppContext);
-
-    const [post, setPost] = useState([])
-
-    const getPosts = async() => {
-
+    const getPosts = async () => {
         try {
-            const response = await axios.get('http://localhost:8080/api/posts')
-
-            const data = response.data
-            setPost(data)
-
-        } catch (e){
-            console.log(e)
+            const response = await axios.get('http://localhost:8080/api/posts');
+            const data = response.data;
+            setPost(data);
+        } catch (e) {
+            console.log(e);
         }
-
     }
 
     useEffect(() => {
-        getPosts()
+        getPosts();
     }, []);
+
+    
 
     function deletePost(id) {
         axios.delete(`http://localhost:8080/api/posts/${id}`, {
@@ -35,54 +32,115 @@ const FeedAxios= () => {
                 Authorization: `Bearer ${token}`
             }
         })
-        setPost(post.filter(post => post._id !== id ))
-        window.location.reload();
+            .then(() => {
+                setPost(prevPost => prevPost.filter(p => p.id !== id));
+            })
+            .catch(error => {
+                console.log("Erro ao excluir post:", error);
+            });
     }
-    
-    
+
+    const handleClick = (post) => {
+        Swal.fire({
+            title: 'Você tem certeza que quer apagar o post?',
+            text: "Você não poderá reveter isso!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sim, apague o post!'
+          }).then((result) => {
+            if (result.isConfirmed) {
+                deletePost(post.id);
+            }
+          })
+    }
+
+
+
+    function formatTextWithLineBreaks(text, maxLength) {
+        let formattedText = '';
+        let cont = 0;
+
+        for (let i = 0; i < text.length; i += maxLength) {
+            formattedText += text.slice(i, i + maxLength);
+            cont += 1
+
+            if (text.length >= 129 && cont >= 3) {
+                formattedText += '...'
+                break;
+            }
+        }
+
+        return formattedText;
+    }
+
+    const toggleReadMore = (index) => {
+        const newPosts = [...post];
+        newPosts[index].showFullContent = !newPosts[index].showFullContent;
+        setPost(newPosts);
+    };
 
     return (
-        <div className="post">
-
+        <div className="post" >
             <p>Bem vindo, {username}</p>
-
             <div className="boxPosts"></div>
             <div className="">
                 <Link to='/post'>
                     <button className="btn-newPost">Nova Publicação</button>
                 </Link>
             </div>
-
-            
-            {post.length === 0 ? (<p className="textboxuser">Carregando...</p>) : (
-                post.map((post) => (
-                    
-                    <div className='BoxPostUser' key={post.id}>
-                        <p>{post.user.login}</p>
-                        <p className="ContentPost">{post.message}</p>
-
-                        {post.user.login === username && 
-                        <div className="btns">
-                            <div className="btn-edit">
-                                <Link to={{ pathname: `/edit/${post.id}` }}>
-                                    <button>Editar</button>
-                                </Link>
+            <div>
+                {post.length === 0 ? (
+                    <p className="textboxuser">Carregando...</p>
+                ) : (
+                    post.map((p, index) => (
+                        <div className='BoxPostUser' key={p.id} >
+                            <p>{p.user.login}</p>
+                            <div className="ContentPost">
+                                {p.showFullContent ? p.message.split('\n').map((line, i) => (
+                                    <React.Fragment key={i}>
+                                        {line}
+                                        <br />
+                                    </React.Fragment>
+                                )) : formatTextWithLineBreaks(p.message, 45)}
+                                <div className='btn-LerMais'>
+                                        {p.message.length >= 129 && (
+                                            <button onClick={() => toggleReadMore(index)}>
+                                                {p.showFullContent ? "Ler menos" : "Ler mais"}
+                                            </button>
+                                        )}
+                                    </div>
                             </div>
-
-                            <div className="btn-delete">
-                                <Link to='/feed'>
-                                    <button onClick={() => deletePost(post.id) }>Deletar</button>
-                                </Link>
+                            
+                                <div className="btns">
+                                    {p.user.login === username &&
+                                        <div className="btn-edit">
+                                            <Link to={{ pathname: `/edit/${p.id}` }}>
+                                                <button>Editar</button>
+                                            </Link>
+                                        </div>
+                                    }
+                                    
+                                    {p.user.login === username &&
+                                        <div className="btn-delete">
+                                            <Link to='/feed'>
+                                                <button onClick={() => handleClick(p)}>Deletar</button>
+                                            </Link>
+                                        </div>
+                                    }
+                                </div>
+                            
+                            <div className="btns">
+                            
                             </div>
+                            
                         </div>
-                        }
-                    </div>
-                
-                ))
-            )}
+                    ))
+                )}
+            </div>
+            
         </div>
-
-
     );
 };
 
